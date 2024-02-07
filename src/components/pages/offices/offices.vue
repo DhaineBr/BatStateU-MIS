3a88910841125f540dd97a0e549ab9bdfd748e0c
Dhaine-Leader <dhaine_br@protonmail.com>
2024-02-07 16:23:45 +0800

    02-07-24 v2

--- src/components/pages/offices/offices.vue
+++ src/components/pages/offices/offices.vue
@@ -4,10 +4,17 @@
     <div class="container">
         <div class="top">
             <div class="left">
-
+                <div class="header">
+                    <div class="head">Report Submission</div>
+                    <div class="caption">Submit report in CSV File with proper documentation and format of data</div>
+                </div>
+                <div class="search-container">
+                    <p></p>
+                    <input type="text">
+                </div>
             </div>
             <div class="right">
-
+                <img src="@/assets/offices.svg" style="width: 25vw">
             </div>
         </div>
         <div class="main-nav">
@@ -32,8 +39,32 @@
 </template>
 
 <style scoped>
+.top    {
+    height: 35vh;
+}
+.left   {
+    float: left;
+    width: 60%;
+    height: inherit;
+}
+.right  {
+    float: right;
+    width: 40%;
+    height: inherit;
+}
 /* Global */
 *   {
+    outline: none;
     border: solid;
+    border-width: 0.01rem;
 }
+.container  {
+    height: 500vh
+}
+.right  {
+    justify-content: center;
+    align-items: center;
+    display: flex;
+}
+
 </style>
--- src/components/pages/profile/profile.vue
+++ src/components/pages/profile/profile.vue
@@ -7,6 +7,21 @@ export default {
         OverallData,
         PbbGrading,
     },
+    data()  { 
+        const progressObjects = [];
+        for (let i = 0; i < 4; i++) {
+        const progressObject = {
+            label:`Q${i + 1}`,
+            value: 90 + i,
+        };
+        progressObjects.push(progressObject);
+        }
+
+        return  {
+            progressObjects,
+            selectedOption: '2023'
+        }
+    },
     methods: {
         goToPbbdashboard() {
         this.$router.push({ name: 'pbbdashboard' });
@@ -43,15 +58,20 @@ export default {
                     <div class="chart-one"><OverallData /></div>
                     <div class="chart-two"><PbbGrading /></div>
                     <div class="chart-three"> 
-                        <select v-model="selectedOption" placeholder="2023">
-                            <option value="option1">Option 1</option>
-                            <option value="option2">Option 2</option>
-                            <option value="option3">Option 3</option>
-                        </select>
-                        <progress max="100" :value="100" class="progress-bar"></progress>
-                        <progress max="100" :value="100" class="progress-bar"></progress>
-                        <progress max="100" :value="50" class="progress-bar"></progress>
-                        <progress max="100" :value="0" class="progress-bar"></progress>
+                        <div class="dropdown">
+                            <select v-model="selectedOption" value="2023">
+                            <option value="2021">2021</option>
+                            <option value="2022">2022</option>
+                            <option value="2023">2023</option>
+                            </select>
+                        </div>
+                        <div class="cont" v-for="(progressObject, index ) in progressObjects" :key="index">
+                            <div class="label">{{ progressObject.label }}</div>
+                            <div class="content">
+                                <progress max="100" :value=progressObject.value class="progress-bar"></progress>
+                                <div class="value">{{ progressObject.value }} %</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -91,9 +111,11 @@ export default {
     height: 15vh;
     padding: 0 0 0 2.5%;
 }
-.card-container {
+.card-container, .content, .progress-bar, .value {
     display: flex;
     flex-direction: row;
+    align-items: center;
+    justify-content: center;
 }
 .card   {
     border-radius: 0.5rem;
@@ -120,11 +142,11 @@ export default {
     color: black
 }
 .perf-stats {
-    height: 40vh;
+    height: 45vh;
     padding: 0 0 0 2.5%;
 }
 .visualization  {
-    height: 65vh;
+    height: 75vh;
 }
 .title  {
     font-weight: 900;
@@ -133,12 +155,12 @@ export default {
     width: 100%;
 }
 .chart-container    {
-    height: 27.5vh;
+    height: 45vh;
     width: 100%;
 }
 .chart-one, .chart-two, .chart-three    {
     margin: auto;
-    height: 25vh;
+    height: 35vh;
 }
 .chart-one, .chart-two  {
     width: 25%
@@ -156,7 +178,7 @@ export default {
     font-size: 1vw;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 0.375rem 0.75rem -0.125rem, rgba(0, 0, 0, 0.3) 0px 0.1875rem 0.4375rem -0.1875rem;
 }
-.title, .summary-header, .header, .campus   {
+.title, .summary-header, .header, .campus, .drop-down   {
     align-items: center;
     justify-content: left;
     display: flex;
@@ -164,18 +186,52 @@ export default {
 .summary-header, .header, .campus   {
     padding: 0 0 0 2.5%;
 }
+select  {
+    color: black;
+    height: 5vh;
+    font-size: 1vw;
+    cursor: pointer;
+}
+.label  {
+    width: 100%;
+    font-size: 1vw;
+    height: 3vh;
+    text-align: left;
+    padding: 0 0 0 1.5vw;
+    color: #000000
+}
+.value  {
+    color: #D00412;
+    float: right;
+    margin: auto;
+    font-size: 1vw;
+}
+.progress-bar   {
+    width: 27.5vw;
+    height: 2vh;
+    float: left;
+    margin: auto
+}
+.progress-bar::-webkit-progress-bar {
+    background-color: #D9D9D9;
+    border-radius: 1rem;
+}
+.progress-bar::-webkit-progress-value {
+    background-color: #D00412;
+    border-radius: 1rem;
+}
 
 /* Right Side */
 .right   {
     width: 25%;
-    height: 90vh;
+    height: 115vh;
     float: right;
 }
 .calendar   {
-    height: 40vh;
+    height: 55vh;
 }
 .activities {
-    height: 50vh;
+    height: 65vh;
     padding: 2.5% 0 0 2.5%;
 }
 .calendar, .activities  {
@@ -184,7 +240,7 @@ export default {
 
 /* Global */
 *   {
-    border: solid;
+    border: none;
     outline: none;
     border-width: 0.01rem;
 }
@@ -201,16 +257,5 @@ export default {
     justify-content: center;
     flex-direction: row;
 }
-.progress-bar   {
-    width: 20vw;
-    height: 2.5vh
-}
-.progress-bar::-webkit-progress-bar {
-    background-color: #D9D9D9;
-    border-radius: 1rem;
-}
-.progress-bar::-webkit-progress-value {
-    background-color: #D00412;
-    border-radius: 1rem;
-}
+
 </style>
